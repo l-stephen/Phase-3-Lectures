4c540f7e8595f2ff432ac5970b2f4b87fe0c337d
stephenlambertfs <stephenolambert@gmail.com>
2023-10-25 10:56:32 -0600

    updated solution

--- 03_oop/lib/pet.py
+++ 03_oop/lib/pet.py
@@ -1,3 +1,25 @@
+#OOP is a programming paradigm based on the concepts of objects or dictionaries
+#For example an object can represent a person and have properties like name, address
+#Behavior or methods like walking, or running
+
+#Benefits
+#Code resuse, modualarity, readable, problem solving, inheritance
+
+#Principles
+# Single Responsibility Principle: 
+    #classes in oop should have one responsibility, and methods should be aligned with that responsibility
+# Separation of concern: 
+    #The idea that the various responsibilities, or concerns, of a computer program should be separated
+# Domain Modeling: 
+    # A method to describe and model entities and the relationships between them
+# Abstraction: 
+    #representing complex real-world problems in a simplified manner by focusing on the most relevant aspects and ignoring the rest.
+# Encapsulation: 
+    #the idea of keeping the internal workings of an object hidden from the outside world.
+# Inheritance: 
+    #the ability for objects to inherit properties and behaviors from other objects.
+# Polymorphism: the ability for objects to take on multiple forms
+
 #Todo 1: Create a dog class, first use pass, and create an instance of the class
 #Then use the __init__ to set attributes, and create an instance of the class
 class Dog:
@@ -30,15 +52,24 @@ class Dog:
         else:
             raise TypeError("Not a number!")
     #Properties
-    #create a property that will get and set the name
-    def get_name(self):
-        print('Getting the name')
+    # #create a property that will get and set the name
+    # def get_name(self):
+    #     print('Getting the name')
+    #     return self._name
+    # def set_name(self,new_name):
+    #     print(f'Setting name to {new_name}')
+    #     self._name = new_name
+    # #To avoid calling get_name and set_name everytime we could use the python decorator property():https://www.programiz.com/python-programming/property 
+    # #This will set some code to a attribute of your class
+    # name = property(get_name, set_name)
+
+    @property
+    def name(self):
         return self._name
-    def set_name(self,new_name):
-        print(f'Setting name to {new_name}')
-    #To avoid calling get_name and set_name everytime we could use the python decorator property():https://www.programiz.com/python-programming/property 
-    #This will set some code to a attribute of your class
-    name = property(get_name, set_name)
+
+    @name.setter
+    def name(self, new_name):
+        self._name = new_name
 
     #Class Methods
     #create a class method that will print all the dogs
@@ -75,8 +106,8 @@ if __name__ == "__main__":
     #Todo 5: Output of the __repr__
     print(tracker)
     #Todo 6: use the property() method, to set and get 
-    brindle.name = "Brind"
-    print(brindle)
+    brindle.name = "Boba"
+    print(brindle.name)
 
     tracker.add_new_species("Labrador")
     print(Dog.species_list)
