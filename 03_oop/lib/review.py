5bfab4b5cec5221ddf8adf61fcf5fd54c369bd09
stephenlambertfs <stephenolambert@gmail.com>
2023-10-25 10:31:10 -0600

    updated solution

--- 03_oop/lib/review.py
+++ 03_oop/lib/review.py
@@ -1,8 +1,11 @@
 #lambda functions are short annoyomous functions
-
-
+add = lambda a,b: a + b
+print(add(10, 20))
 # You can pass a lambda function to a higher order function
-
+def multiply(num):
+    return lambda n: num * n
+double = multiply(10)
+print(double(10))
 
 #raising an error
 # val = False
@@ -14,4 +17,8 @@
 #everything in python is an object
 # when you create something python will allocate memory and assign an id
 
-
+x = []
+y = []
+print(x == y)
+print(id(type(x)))
+print(id(list))
