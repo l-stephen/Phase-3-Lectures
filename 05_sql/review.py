10797a38e31bca242af777b1d3b5e27af94233c5
stephenlambertfs <stephenolambert@gmail.com>
2023-10-30 10:24:04 -0600

    updated solution

--- 05_sql/review.py
+++ 05_sql/review.py
@@ -1,19 +1,62 @@
 # One to One
-#Create a one to one relationship between apartments and tennets
-#Apartments should be initialized with name, location, and a tenet that is set to none
-#Create Methods get_tenent that gets the tenent, and set_tenent to set a new tenent
+class Apartment:
+    def __init__(self,name,location):
+        self.name = name
+        self.location = location
+        self.tenent = None
+    def get_tenent(self):
+        return self._tenent
+    # Review how to change relationships dynamically
+    def set_tenent(self,newtenent):
+        if type(newtenent) is Tenent:
+            # If self._tenent exist
+            if self._tenent is not None:
+                self._tenent.apartment = None
+            newtenent.apartment = self
+            self._tenent = newtenent
+        else:
+            self._tenent = None
+    tenent = property(get_tenent,set_tenent)
+
+class Tenent:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+        self.apartment = None
+
+david = Tenent("David",25)
+sunnyside = Apartment("Sunnyside","Boulder")
+tanner = Tenent("Tanner",25)
+sunnyside.tenent = david
+sunnyside.tenent = tanner
 
 
+# One to Many
+class Team:
+    def __init__(self,name,mascot):
+        self.name = name
+        self.mascot = mascot
+        self.players = []
+    def addPlayer(self,newplayer):
+        self.players.append(newplayer)
+        newplayer.team = self
+class Player:
+    def __init__(self,name):
+        self.name = name
+        self.team = None
+player1 = Player("Guy")
+player2 = Player("Other Guy")
+team1 = Team("Good team","Is not the author of ready player one")
+team1.addPlayer(player1)
 
-#One to Many
-#Create a one to many relationship between a Team and Player
-#Team should be initialized with a name, mascot, and have an attrivute players that is a list
-#Player should be initilized with a name
-#Create a method add_player to add a player to the players list
-
-
-#Many to Many
-#Create a many to many relation ship between a teacher and student
-#Teacher should be initialized with name, subject, and have a list of students
-#Student should be initialized with a name, grade, and have a list of teachers
-
+# Many to Many
+class Teacher:
+    def __init__(self,name,subject):
+        self.name = name
+        self.subject = subject
+        self.students = []
+class Student:
+    def __init__(self,name,grade):
+        self.name = name
+        self.grade = grade
+        self.teachers = []
