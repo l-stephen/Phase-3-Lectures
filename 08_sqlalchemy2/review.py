8ca37810a225f1d2aa039a5a10219271db5e2021
Stephen Lambert <97977357+stephen-lambe@users.noreply.github.com>
2023-11-03 13:43:57 -0600

    Update review.py

--- 08_sqlalchemy2/review.py
+++ 08_sqlalchemy2/review.py
@@ -1,43 +1,31 @@
-# Imports
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.orm import Session, declarative_base, validates
-#initialize with decrative base
+
 Base = declarative_base()
 
-#Create tables
 class Dog(Base):
-    __tablename__ = "dogs_table"
+    __tablename__ = "dogs"
     id = Column(Integer, primary_key = True)
     name = Column(String, nullable = False)
     age = Column(Integer)
     amount_eaten = Column(Integer)
 
-    @validates("age")
-    def validate_age(self,key,value):
-        if type(value) is int and value >= 0:
+    @validates
+    def validate_age(self, key, value):
+        if isinstance(value, int) and value >= 0:
             return value
         else:
-            raise ValueError("Not valid age")
-    
+            raise ValueError("Must be greater than 0")
+        
     def __repr__(self):
         return f'{self.name}'
-
-if __name__ == '__main__':
-    #Create the engine
+    
+if  __name__ == "__main__":
     engine = create_engine('sqlite:///animals.db')
-    # Dog.__table__.drop(engine)
     Base.metadata.create_all(engine)
-
     with Session(engine) as session:
-        dog = Dog(name = "Tracker", age=3, amount_eaten = 2)
-        dog2 = Dog(name = "Tracker2", age=10, amount_eaten = 2)
-        session.add_all([dog,dog2])
+        dog = Dog(name="Tracker", age = 5, amount_eaten = 2)
+        session.add(dog)
         session.commit()
-        all_dogs = session.query(Dog).filter(Dog.name == "Tracker").all()
-        # print(all_dogs)
-        one_dog = session.query(Dog).filter(Dog.id ==2).first()
-        one_dog.age = 20
-        session.add(one_dog)
-        session.commit()
-        session.delete(one_dog)
-        session.commit()
+
+    
